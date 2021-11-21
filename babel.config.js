fd551c11e4e2d4f0c104f789094df6af8f9b854b
Vadim Vengriniuc <54371115+Vadim179@users.noreply.github.com>
2021-11-21 12:59:59 +0200

    config update

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./app'],
+          extensions: ['.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@tests': 'app/tests',
+            '@theme': 'app/theme',
+            '@types': 'app/types',
+            '@context': 'app/context',
+            '@utils': 'app/utils',
+            '@styles': 'app/styles',
+            '@navigation': 'app/navigation',
+            '@components': 'app/components',
+            '@icons': 'app/components/assets/icons',
+            '@screens': 'app/screens',
+          },
+        },
+      ],
+    ],
   };
 };
