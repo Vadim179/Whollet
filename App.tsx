8e6c52f14a65fe67ec59f5cd51486619ae1047f8
Vadim Vengriniuc <54371115+Vadim179@users.noreply.github.com>
2021-11-21 13:28:30 +0200

    Fonts -> Added expo font loading functionality

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useFonts } from 'expo-font';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { ContextProvider } from './app/context';
@@ -6,6 +7,17 @@ import { DarkThemeProvider, NotificationHandler } from './app/components';
 import { RootNavigator } from './app/navigation';
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    ['TitilliumWeb-Bold']: require('./assets/fonts/TitilliumWeb-Bold.ttf'),
+    ['TitilliumWeb-Light']: require('./assets/fonts/TitilliumWeb-Light.ttf'),
+    ['TitilliumWeb-Regular']: require('./assets/fonts/TitilliumWeb-Regular.ttf'),
+    ['TitilliumWeb-SemiBold']: require('./assets/fonts/TitilliumWeb-SemiBold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <ContextProvider>
       <DarkThemeProvider>
